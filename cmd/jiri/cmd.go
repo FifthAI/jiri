fd2db9f0c3b81c6733ab32ff716d11390e5d045f
fd <shadowcat.liuyang@gmail.com>
2019-11-18 10:19:54 +0800

    fix for win

--- cmd/jiri/cmd.go
+++ cmd/jiri/cmd.go
@@ -8,32 +8,14 @@
 package main
 
 import (
-	"fmt"
 	"runtime"
-	"syscall"
-
 	"fuchsia.googlesource.com/jiri/cmdline"
 )
 
 func init() {
-	cmdRoot = newCmdRoot()
 	runtime.GOMAXPROCS(runtime.NumCPU())
-	if runtime.GOOS == "darwin" {
-		var rLimit syscall.Rlimit
-		if err := syscall.Getrlimit(syscall.RLIMIT_NOFILE, &rLimit); err != nil {
-			fmt.Println("Warning: unable to obtain rlimit: ", err)
-			return
-		}
-		// The max file limit is 10240 for osx, even though
-		// the max returned by Getrlimit is 1<<63-1.
-		// This is defined in OPEN_MAX in sys/syslimits.h.
-		if rLimit.Cur < 10240 {
-			rLimit.Cur = 10240
-			if err := syscall.Setrlimit(syscall.RLIMIT_NOFILE, &rLimit); err != nil {
-				fmt.Println("Warning: unable to increase rlimit: ", err)
-			}
-		}
-	}
+	// darwin rlimit manipulation moved to conditional compilation
+	cmdRoot = newCmdRoot()
 }
 
 func main() {
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module fuchsia.googlesource.com/jiri
 go 1.13
 
 require (
+	golang.org/x/crypto v0.0.0-20190308221718-c2843e01d9a2
 	golang.org/x/net v0.0.0-20190909003024-a7b16738d86b
 	golang.org/x/sync v0.0.0-20190423024810-112230192c58
 )
